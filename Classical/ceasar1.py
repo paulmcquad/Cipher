22b2758389bdb714bef94255b543ad620d4bb73b
Paul McQuade <paulmcquad@gmail.com>
2023-01-05 21:58:49 +0000

    Fixed Decrypt
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- Classical/ceasar1.py
+++ Classical/ceasar1.py
@@ -11,6 +11,32 @@ def ceasar(text, shift, alphabets):
     table = str.maketrans(final_alphabet, final_shifted_alphabet)
     return text.translate(table)
 
+def ceasar_decrypt(text, shift, alphabets):
+    """Decrypt text."""
+    ALPHABET = string.ascii_letters
+    ALPHABET_LENGTH = len(alphabets)
+    ALPHABET_LENGTH_NEGATIVE = - ALPHABET_LENGTH
+
+    decrypted_letters = []
+    for letter in text:
+        if letter not in ALPHABET:
+            decrypted_letters.append(letter)
+        elif letter in ALPHABET:
+            plain_letter_index = ALPHABET.index(letter)
+            decrypted_letter_index = plain_letter_index - shift
+            if decrypted_letter_index >= 0:
+                decrypted_letters.append(ALPHABET[decrypted_letter_index])
+            else:
+                try:
+                    decrypted_letter_index += ALPHABET_LENGTH
+                    decrypted_letters.append(ALPHABET[decrypted_letter_index])
+                except IndexError:
+                        while decrypted_letter_index <= ALPHABET_LENGTH_NEGATIVE:
+                            decrypted_letter_index = decrypted_letter_index + ALPHABET_LENGTH
+                        decrypted_letters.append(ALPHABET[decrypted_letter_index])
+    decrypted_message = ''.join(decrypted_letters)
+    return decrypted_message
+
 
 if __name__ == "__main__":
 #    print(ceasar(plain_text, 7, [string.ascii_lowercase,string.ascii_uppercase, string.punctuation]))
@@ -20,12 +46,12 @@ if __name__ == "__main__":
         case 1:
             key = int(input("Key: 1- 25: "))
             secret_message = input("Encode:\n")
-            print(ceasar(secret_message, key, [string.ascii_lowercase,string.ascii_uppercase, string.punctuation]))
+            print(ceasar(secret_message, key, [string.ascii_letters, string.punctuation]))
         case 2:
             key = int(input("Key: 1- 25: "))
             ciphertext_to_decipher = input("Decode:\n")
-            print(ceasar(ciphertext_to_decipher, key, [string.ascii_lowercase,string.ascii_uppercase, string.punctuation]))
+            print(ceasar_decrypt(ciphertext_to_decipher, key, [string.ascii_lowercase,string.ascii_uppercase, string.punctuation]))
         case 3:
             secret_message = input("Brute Force:\n")
             for key in range(26):
-                print("Key {0}:".format(key), ceasar(secret_message, key, [string.ascii_lowercase,string.ascii_uppercase, string.punctuation]))
+                print("Key {0}:".format(key), ceasar(secret_message, key, [string.ascii_letters, string.punctuation]))
